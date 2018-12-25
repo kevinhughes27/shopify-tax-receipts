cbabe488c49a40eb445742c8ee4f360e90872ca9
Kevin Hughes <kevinhughes27@gmail.com>
2018-12-25 19:05:28 +0700

    config sidekiq for production env

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,2 +1,15 @@
 require 'sidekiq'
-require 'redis'
+
+redis_url = if ENV['DEVELOPMENT']
+  'redis://localhost:6379'
+else
+  ENV['REDIS_URL']
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: redis_url }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: redis_url }
+end
