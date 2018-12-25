eac0e55f423779d0f5cd8ad61fe443da0a56443c
Kevin Hughes <kevinhughes27@gmail.com>
2018-12-25 20:10:10 +0700

    update for new api

--- src/routes/webhooks.rb
+++ src/routes/webhooks.rb
@@ -2,14 +2,14 @@ require 'sinatra/shopify-sinatra-app'
 
 class SinatraApp < Sinatra::Base
   get '/webhooks' do
-    shopify_session do
+    shopify_session do |shop_name|
       @webhooks = ShopifyAPI::Webhook.all
       erb :webhooks
     end
   end
 
   # delete '/webhooks' do
-  #   shopify_session do
+  #   shopify_session do |shop_name|
   #     ShopifyAPI::Webhook.find(params["id"]).destroy
   #     flash[:notice] = "Webhook Removed"
   #     redirect '/webhooks'
