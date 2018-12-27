28bb4b902625bb6282805df8b58157810cbb2a1b
Kevin Hughes <kevinhughes27@gmail.com>
2018-12-27 16:34:07 +0700

    fix seed

--- test/support/seed.rb
+++ test/support/seed.rb
@@ -1,4 +1,9 @@
 def seed_db
+  Shop.delete_all
+  Charity.delete_all
+  Product.delete_all
+  Donation.delete_all
+
   apple = Shop.create!(name: 'apple.myshopify.com', token: 'token')
   Charity.create!(shop: apple.name, name: 'Amnesty', charity_id: 12345)
   Product.create(shop: apple.name, product_id: 632910392)
