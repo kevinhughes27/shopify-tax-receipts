1f23f7ec08056adac33f9f12d930ebe58b88298a
Kevin Hughes <kevinhughes27@gmail.com>
2019-01-10 11:58:16 +0700

    convert to rake task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_025434) do
+ActiveRecord::Schema.define(version: 2018_12_28_050903) do
 
   create_table "charities", force: :cascade do |t|
     t.string "name", limit: 255
