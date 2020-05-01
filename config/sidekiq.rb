a42b1c6277b45e52730fa53727206ac5cf344ba6
Kevin Hughes <kevinhughes27@gmail.com>
2020-05-01 19:04:18 -0400

    try increasing redis timeoutfor sidekiq

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -7,9 +7,9 @@ else
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
+  config.redis = { url: redis_url, network_timeout: 5 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
+  config.redis = { url: redis_url, network_timeout: 5 }
 end
