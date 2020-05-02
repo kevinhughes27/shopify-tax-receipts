8d33481ac783470c76b354c3a04ca6bd6111d7b0
Kevin Hughes <kevinhughes27@gmail.com>
2020-05-02 10:58:20 -0400

    timeout did not help

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -7,9 +7,9 @@ else
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url, network_timeout: 5 }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url, network_timeout: 5 }
+  config.redis = { url: redis_url }
 end
