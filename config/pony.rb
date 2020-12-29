f8169607db0604b79bfeefd13432a221153337dd
Kevin Hughes <kevinhughes27@gmail.com>
2020-12-29 13:13:57 -0500

    provision mailgun

--- config/pony.rb
+++ config/pony.rb
@@ -12,16 +12,27 @@ class SinatraApp < Sinatra::Base
       :via_options => {:location => File.expand_path('../../tmp/letter_opener', __FILE__)}
     }
   else
+    # Pony.options = {
+    #   :via => :smtp,
+    #   :via_options => {
+    #     :address => 'smtp.sendgrid.net',
+    #     :port => '587',
+    #     :domain => 'heroku.com',
+    #     :user_name => 'apikey',
+    #     :password => ENV['SENDGRID_API_KEY'],
+    #     :authentication => :plain,
+    #     :enable_starttls_auto => true
+    #   }
+    # }
     Pony.options = {
       :via => :smtp,
       :via_options => {
-        :address => 'smtp.sendgrid.net',
-        :port => '587',
-        :domain => 'heroku.com',
-        :user_name => 'apikey',
-        :password => ENV['SENDGRID_API_KEY'],
+        :port           => ENV['MAILGUN_SMTP_PORT'],
+        :address        => ENV['MAILGUN_SMTP_SERVER'],
+        :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+        :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+        :domain         => 'taxreceipts.heroku.com',
         :authentication => :plain,
-        :enable_starttls_auto => true
       }
     }
   end
