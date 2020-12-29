85ceb44878105b348bfb459cab0f2f88efc681a7
Kevin Hughes <kevinhughes27@gmail.com>
2020-12-29 14:53:15 -0500

    turn on mailgun again

--- config/pony.rb
+++ config/pony.rb
@@ -12,28 +12,28 @@ class SinatraApp < Sinatra::Base
       :via_options => {:location => File.expand_path('../../tmp/letter_opener', __FILE__)}
     }
   else
-    Pony.options = {
-      :via => :smtp,
-      :via_options => {
-        :address => 'smtp.sendgrid.net',
-        :port => '587',
-        :domain => 'heroku.com',
-        :user_name => 'apikey',
-        :password => ENV['SENDGRID_API_KEY'],
-        :authentication => :plain,
-        :enable_starttls_auto => true
-      }
-    }
     # Pony.options = {
     #   :via => :smtp,
     #   :via_options => {
-    #     :port           => ENV['MAILGUN_SMTP_PORT'],
-    #     :address        => ENV['MAILGUN_SMTP_SERVER'],
-    #     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    #     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    #     :domain         => 'taxreceipts.heroku.com',
+    #     :address => 'smtp.sendgrid.net',
+    #     :port => '587',
+    #     :domain => 'heroku.com',
+    #     :user_name => 'apikey',
+    #     :password => ENV['SENDGRID_API_KEY'],
     #     :authentication => :plain,
+    #     :enable_starttls_auto => true
     #   }
     # }
+    Pony.options = {
+      :via => :smtp,
+      :via_options => {
+        :port           => ENV['MAILGUN_SMTP_PORT'],
+        :address        => ENV['MAILGUN_SMTP_SERVER'],
+        :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+        :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+        :domain         => 'shopify-taxreceipts.com',
+        :authentication => :plain,
+      }
+    }
   end
 end
